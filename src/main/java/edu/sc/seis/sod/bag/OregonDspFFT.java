9255f5a38b8b2ccd28f36a8b74df408785d81a06
Philip Crotwell <crotwell@seis.sc.edu>
2017-09-19 14:22:42 -0400

    fix N vs log2N for fft

--- src/main/java/edu/sc/seis/sod/bag/OregonDspFFT.java
+++ src/main/java/edu/sc/seis/sod/bag/OregonDspFFT.java
@@ -5,66 +5,104 @@ import com.oregondsp.signalProcessing.fft.RDFT;
 public class OregonDspFFT {
 
     public static float[] forward(float[] realData) {
-        RDFT rdft = new RDFT((int)Math.round(Math.log(realData.length) / Math.log(2)));
-        float[] forward = new float[realData.length];
-        rdft.evaluate(realData, forward);
-        return forward;
+        int N = 32;
+        int log2N = 5;
+        while(N < realData.length) { log2N +=1; N *= 2;}
+        float[] in = new float[N];
+        float[] out = new float[N];
+        System.arraycopy(realData, 0, in, 0, realData.length);
+        RDFT rdft = new RDFT(log2N);
+        rdft.evaluate(in, out);
+        return out;
     }
 
     /**
      * Performs the inverse fft operation of the realFFT call.
      */
     public static float[] inverse(float[] realData) {
-        RDFT rdft = new RDFT((int)Math.round(Math.log(realData.length) / Math.log(2)));
-        float[] inverse = new float[realData.length];
-        rdft.evaluateInverse(realData, inverse);
-        return inverse;
+        int N = 32;
+        int log2N = 5;
+        while(N < realData.length) { log2N +=1; N *= 2;}
+        float[] in = new float[N];
+        float[] out = new float[N];
+        System.arraycopy(realData, 0, in, 0, realData.length);
+        RDFT rdft = new RDFT(log2N);
+        rdft.evaluateInverse(in, out);
+        return out;
     }
 
     public static float[] correlate(float[] x, float[] y) {
-        float[] xforward = new float[x.length];
-        xforward = forward(x);
-        float[] yforward = new float[y.length];
-        yforward = forward(y);
-        float[] ans = new float[x.length];
+        int N = 32;
+        int log2N = 5;
+        while(N < x.length) { log2N +=1; N *= 2;}
+        while(N < y.length) { log2N +=1; N *= 2;}
+        float[] xIn = new float[N];
+        float[] yIn = new float[N];
+        System.arraycopy(x, 0, xIn, 0, x.length);
+        System.arraycopy(y, 0, yIn, 0, y.length);
+        RDFT rdft = new RDFT(log2N);
+        float[] xforward = new float[N];
+        rdft.evaluate(xIn, xforward);
+        xIn = null; // memory
+        float[] yforward = new float[N];
+        rdft.evaluate(yIn, yforward);
+        yIn = null; // memory
+        float[] ans = new float[N];
         // handle 0 and nyquist
         ans[0] = xforward[0] * yforward[0];
-        ans[x.length / 2] = xforward[x.length / 2] * yforward[x.length / 2];
+        ans[N / 2] = xforward[N / 2] * yforward[N / 2];
         float a, b, c, d;
-        for (int j = 1; j < x.length / 2; j++) {
+        for (int j = 1; j < N / 2; j++) {
             a = xforward[j];
-            b = xforward[x.length - j];
+            b = xforward[N - j];
             c = yforward[j];
-            d = yforward[x.length - j];
+            d = yforward[N - j];
             ans[j] = a * c + b * d;
-            ans[x.length - j] = -a * d + b * c;
+            ans[N - j] = -a * d + b * c;
         }
-        ans = inverse(ans);
-        return ans;
+        xforward = null;
+        yforward = null;
+        float[] out = new float[N];
+        rdft.evaluateInverse(ans, out);
+        return out;
     }
 
     public static float[] convolve(float[] x, float[] y, float delta) {
-        float[] xforward = new float[x.length];
-        xforward = forward(x);
-        float[] yforward = new float[y.length];
-        yforward = forward(y);
-        float[] ans = new float[x.length];
+        int N = 32;
+        int log2N = 5;
+        while(N < x.length) { log2N +=1; N *= 2;}
+        while(N < y.length) { log2N +=1; N *= 2;}
+        float[] xIn = new float[N];
+        float[] yIn = new float[N];
+        System.arraycopy(x, 0, xIn, 0, x.length);
+        System.arraycopy(y, 0, yIn, 0, y.length);
+        RDFT rdft = new RDFT(log2N);
+        float[] xforward = new float[N];
+        rdft.evaluate(xIn, xforward);
+        xIn = null; // memory
+        float[] yforward = new float[N];
+        rdft.evaluate(yIn, yforward);
+        yIn = null; // memory
+        
+        
+        float[] ans = new float[N];
         // handle 0 and nyquist
         ans[0] = xforward[0] * yforward[0];
-        ans[x.length / 2] = xforward[x.length / 2] * yforward[x.length / 2];
+        ans[N / 2] = xforward[N / 2] * yforward[N / 2];
         float a, b, c, d;
-        for (int j = 1; j < x.length / 2; j++) {
+        for (int j = 1; j < N / 2; j++) {
             a = xforward[j];
-            b = xforward[x.length - j];
+            b = xforward[N - j];
             c = yforward[j];
-            d = yforward[x.length - j];
+            d = yforward[N - j];
             ans[j] = a * c - b * d;
-            ans[x.length - j] = a * d + b * c;
+            ans[N - j] = a * d + b * c;
         }
-        ans = inverse(ans);
+        float[] out = new float[N];
+        rdft.evaluateInverse(ans, out);
         for (int i = 0; i < ans.length; i++) {
-            ans[i] *= delta;
+            out[i] *= delta;
         }
-        return ans;
+        return out;
     }
 }
