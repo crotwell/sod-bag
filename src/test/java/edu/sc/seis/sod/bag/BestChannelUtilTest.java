310fcc61bef68b5bfadabf4316ec1fa5a0ee8902
Philip Crotwell <crotwell@seis.sc.edu>
2020-02-19 16:00:34 -0500

    test cases

---
+++ src/test/java/edu/sc/seis/sod/bag/BestChannelUtilTest.java
@@ -0,0 +1,19 @@
+package edu.sc.seis.sod.bag;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BestChannelUtilTest {
+
+    @Test
+    public void testGetBestChannel() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetBestMotionVector() {
+        fail("Not yet implemented");
+    }
+
+}
